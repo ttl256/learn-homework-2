85a73bdecc1647c2c24c24b3efb8c636ba9db4f7
ttl256 <64251319+ttl256@users.noreply.github.com>
2020-12-03 00:39:21 +0300

    1_date_and_time is done

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,21 +7,21 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import date, timedelta, datetime
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-
+    delta1 = timedelta(days=1)
+    delta30 = timedelta(days=30)
+    today = date.today()
+    date1 = today - delta1
+    date2 = today - delta30
+    print(f"Today is {today}")
+    print(f"Yesterday it was {date1}")
+    print(f"30 days ago it was {date2}")
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    datetime_obj = datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
+    return datetime_obj
 
 if __name__ == "__main__":
     print_days()
